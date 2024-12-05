f24bf3e5d3fe7a5bcfb5478bf94d5b373253daf7
IYahirMP <ivanyahirmopi@gmail.com>
2024-12-05 17:18:37 -0600

    Back to zero square

--- resources/views/admin/adminLayout.blade.php
+++ resources/views/admin/adminLayout.blade.php
@@ -49,6 +49,37 @@
                 </x-header.dropdown-menu-option>
             </x-slot:options>
         </x-header.dropdown-menu>
+        <x-header.dropdown-menu>
+            <x-slot:header>Operaciones</x-slot:header>
+            <x-slot:content>
+                Especies
+                <span class="ms-2 w-2 [&>svg]:h-5 [&>svg]:w-5">
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor">
+                    <path
+                        fill-rule="evenodd"
+                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                        clip-rule="evenodd" />
+                  </svg>
+                </span>
+            </x-slot:content>
+            <x-slot:options>
+                <x-header.dropdown-menu-option>
+                    <x-slot:link>#</x-slot:link>
+                    <x-slot:name>&iacute;ndice</x-slot:name>
+                </x-header.dropdown-menu-option>
+                <x-header.dropdown-menu-option>
+                    <x-slot:link>admin/species/indice</x-slot:link>
+                    <x-slot:name>Buscar</x-slot:name>
+                </x-header.dropdown-menu-option>
+                <x-header.dropdown-menu-option>
+                    <x-slot:link>#</x-slot:link>
+                    <x-slot:name>Crear</x-slot:name>
+                </x-header.dropdown-menu-option>
+            </x-slot:options>
+        </x-header.dropdown-menu>
     </x-slot:navigation>
     <x-slot:rightButton>
         <div
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ObservationController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\TaxonomicBlockController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,6 +29,19 @@ Route::controller(ObservationController::class)
         Route::put('/{id}', 'update')->name('update');
         Route::delete('/{id}', 'destroy')->name('destroy');
         Route::post('/{id}/editar', 'edit')->name('edit');
+    });
+
+Route::controller(TaxonomicBlockController::class)
+    ->prefix('admin/taxon')
+    ->group(function () {
+        Route::get('/species', 'getSpecies')->name('getSpecies');
+        Route::get('/', 'index')->name('index');
+        Route::post('/', 'store')->name('store');
+        Route::get('/{id}', 'show')->name('show');
+        Route::put('/{id}', 'update')->name('update');
+        Route::delete('/{id}', 'destroy')->name('destroy');
+        Route::post('/{id}/editar', 'edit')->name('edit');
+        Route::get('/{id}/jerarquia', 'getHierarchy')->name('getHierarchy');
     });
 
 Route::controller(AdminController::class)
